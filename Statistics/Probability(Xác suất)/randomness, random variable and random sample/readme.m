d24accd07028f071f786263c1c229fa8a602a2d8
DatN_Sen <72129432+tandat-1305@users.noreply.github.com>
2022-05-02 10:57:37 +0700

    Update readme.m

--- Statistics/Probability(Xác suất)/randomness, random variable and random sample/readme.m
+++ Statistics/Probability(Xác suất)/randomness, random variable and random sample/readme.m
@@ -1,1 +1,46 @@
+Discrete random cariables 
+1. A random variable is a variable whose value is a numerical outcome of a random phenomenon.
+Example: A basketball player shoots three free throws. We define the random vaeiable X as the number of baskets successfully made.
 
+2. A discrete random variable X has a finite number of possible values
+Example: A basketball player shoots three free throws. The number of baskets successfully made is a discreta random variable (X). Xa can only take the values 0, 1, 2 or 3.
+
+The probability distribution of a random variable X lists the values and their probabilities:
+The probabilities p, must add up to 1.
+Example: A basketball player shoots three free throws. The random variable X is the number of baskets successfully made.
+
+The probability of any event is the sum of the probabilities p; of the values of X that make up the event.
+Example: A basketball player shoots three free throws. The random variable X is the number of baskets successfully made.
+Value of X  |0      1     2     3
+------------|-------------------------
+Probability |1/8    3/8   3/8   1/8
+What is the probability that the player successfully makes at least two baskets ("at least two" means "two or more")?
+P(X>=2) = P(X=2) + P(X=3) = 3/8 + 1/8 = 1/2
+What is the probability that the player successfully makes fewer than three baskets?
+P(X<3) = P(X=0) + P(X=1) + P(X=2) = 1/8 + 3/8 + 3/8 = 7/8 or
+P(X<3) 1- P(X-3) = 1-1/8 = 7/8
+
+-----------------------------
+Vietnamese language
+Biến ngẫu nhiên rời rạc
+
+1. Biến ngẫu nhiên là một biến có giá trị là kết quả số của một hiện tượng ngẫu nhiên.
+Ví dụ: Một cầu thủ bóng rổ ném ba quả ném phạt. Chúng tôi xác định X vaeiable ngẫu nhiên là số giỏ được tạo thành công.
+
+2. Một biến ngẫu nhiên rời rạc X có một số hữu hạn các giá trị có thể có
+Ví dụ: Một cầu thủ bóng rổ ném ba quả ném phạt. Số lượng rổ được thực hiện thành công là một biến ngẫu nhiên (X). Xa chỉ có thể nhận các giá trị 0, 1, 2 hoặc 3.
+
+Phân phối xác suất của một biến ngẫu nhiên X liệt kê các giá trị và xác suất của chúng:
+Xác suất p, phải thêm đến 1.
+Ví dụ: Một cầu thủ bóng rổ ném ba quả ném phạt. Biến ngẫu nhiên X là số rổ được thực hiện thành công.
+
+Xác suất của bất kỳ sự kiện nào là tổng các xác suất p; trong số các giá trị của X tạo nên sự kiện.
+Ví dụ: Một cầu thủ bóng rổ ném ba quả ném phạt. Biến ngẫu nhiên X là số rổ được thực hiện thành công.
+Giá trị  X  |0      1     2     3
+------------|-------------------------
+Xác suất    |1/8    3/8   3/8   1/8
+Xác suất để người chơi thực hiện thành công ít nhất hai rổ ("ít nhất hai" có nghĩa là "hai hoặc nhiều hơn")?
+P(X>=2) = P(X=2) + P(X=3) = 3/8 + 1/8 = 1/2
+Xác suất người chơi ném thành công ít hơn ba rổ là bao nhiêu?
+P(X<3) = P(X=0) + P(X=1) + P(X=2) = 1/8 + 3/8 + 3/8 = 7/8 or
+P(X<3) 1- P(X-3) = 1-1/8 = 7/8

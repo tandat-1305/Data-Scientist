b96dd55b5218f2b094cee0f043da90d3440b617b
DatN_Sen <72129432+tandat-1305@users.noreply.github.com>
2022-05-02 19:22:29 +0700

    Create readme.m

---
+++ Statistics/Hypothesis Testing/p-value/readme.m
@@ -0,0 +1,40 @@
+I. P value là gì?
+• Định nghĩa: P value (giá trị P) là xác xuất có thể quan sát được vấn đề nghiên cứu trong mẫu nghiên cứu khi giả thuyết vô hiệu (Ho) là đúng.
+P value = P (A | Ho)
+A: vấn đề muốn nghiên cứu (bệnh, hiệu quả thuốc..)
+Ho: Giả thuyết vô hiệu
+
+II. P value liên quan đến phân phối xác xuất
+Thường hay được lấy ví dụ đối với phân phối chuẩn.
+Tuy nhiên, còn rất nhiều các phân phối khác nữa:
+    Phân phối t
+    Phân phối nhị phân
+    Phân phối Khi bình phương
+    Phân phối poission
+P value sẽ được tính bằng máy tính, nhưng cũng có thể tính tay rồi tra bảng.
+
+III. P value chính là giá trị xác suất
+Vì thế, P value: sẽ nằm trong khoảng từ 0 đến 1
+Hoặc, quy đổi ra tỷ lệ %, sẽ từ 0% đến 100%
+Kiểm định: P value sẽ được so sánh với mức ý nghĩa thống kê quy định trước (a = 0,05).
+a còn được biết đến trong tiếng Anh là Critical Value
+
+IV. VÍ DỤ
+Quay lại với ví dụ P=0,03
+Giả sử chúng ta muốn kiểm định sự khác về thời gian nằm viện của nhóm bệnh nhân điều trị và nhóm đối chứng.
+Với kết quả P=0,03 thì có thể hiểu như sau:
+Nếu sự khác biệt về thời gian nằm viện giữa hai nhóm là không có, thì chỉ 3% khả năng ghi nhận được sự khác biệt với mẫu nghiên của chúng ta.
+Diễn giải xa hơn: Khả năng quan sát thấy là 3%, giá trị này <ngưỡng ý nghĩa thống kê 5% (0,05) -> Bác bỏ Ho và chấp nhận Ha
+
+Nếu P=0,08 thì sao?
+Tiếp theo ví dụ trước
+Nếu sự khác biệt về thời gian nằm viện giữa hai nhóm là không có, thì tới 8% khả năng ghi nhận được sự khác biệt với mẫu nghiên của chúng ta.
+Diễn giải xa hơn: Khả năng quan sát thấy là 8%, giá trị này >ngưỡng ý nghĩa thống kê 5% (0,05) -> Không bác bỏ được Ho, nên chấp nhận Ho, và bác bỏ Ha
+
+Hiểu một cách đơn giản về P value
+P value chính là giá trị của xác suất.
+Máy tính sẽ giúp chúng ta tỉnh P value.
+Khi có P value và quy định ngưỡng ý nghĩa thống kê (0,05)
+p< 0,05: Bác bỏ giả thuyết Ho -> Chấp nhận Ha
+p> 0,05: Không thể bác bỏ giả thuyết Ho -> Bắc bỏ Ha
+
